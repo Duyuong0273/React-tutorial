6e3429801f691f9801bf7eb2d677d40056c3b0e9
Hoangduy273 <duyuong0273@gmail.com>
2024-11-12 10:32:13 +0700

    learn State

--- react-basic/src/assets/view/Example/MyComponents.js
+++ react-basic/src/assets/view/Example/MyComponents.js
@@ -1,8 +1,38 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  //key: value
+  state = {
+    name: "Duy",
+    age: 21,
+  };
+
+  /**
+   * JSX => return block
+   * fragment
+   */
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>Hello my Component</div>;
+    // let name = "Duy";
+
+    return (
+      <div className="abc">
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          {console.log("My name is: ", this.state.name)} Hello my Component, my
+          name is {this.state.name}
+        </div>
+        <div className="second">I {this.state.age} year old</div>
+      </div>
+    );
   }
 }
 
